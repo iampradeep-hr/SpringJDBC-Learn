ba7ec0f01f987996f85d8ebfd7ec38207b1bf02a
Pradeep <iampradeephr@gmail.com>
2023-02-28 23:19:55 +0530

    Initial Commit

---
+++ src/main/java/com/pradeep/App.java
@@ -0,0 +1,17 @@
+package com.pradeep;
+
+
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.DriverManager;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+
+
+
+        System.out.println( "Hello World!" );
+    }
+}
